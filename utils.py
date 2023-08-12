accfaa359655bd37430f7040f8ab209471621ec1
NguyenThanhAI <Thanh.CEAA.K58.HUST@gmail.com>
2023-08-12 17:58:48 +0700

    Add preprocess function

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
-
+import math
 from typing import List, Tuple, Optional
 import numpy as np
+
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
 
 
 def pca(x: np.ndarray, alpha: float=0.95) -> Tuple[np.ndarray, np.ndarray]:
@@ -96,3 +100,110 @@ def kernel_pca(x: np.ndarray, alpha: float=0.95, type: str="gaussian_rbf", sigma
     #print(reduced_data)
 
     return C, reduced_data
+
+
+def preprocess_data(df: pd.DataFrame, remove_email_null: bool=True, use_text_categorical: bool=True, one_hot_encode: bool=True) -> Tuple[np.ndarray, np.ndarray]:
+    columns_to_check = ['educ', 'wrkstat']
+    df.dropna(subset=columns_to_check, inplace=True)
+    df['emailtotal'] = np.where(df['emailhr'].isna() | df['emailmin'].isna(), np.nan, df['emailhr'] * 60 + df['emailmin'])
+    df = df.drop(['emailhr', 'emailmin'], axis=1)
+
+    if remove_email_null:
+        df.dropna(subset=['emailtotal'], inplace=True)
+    else:
+        df = df[df['emailtotal'].isna()]
+        df = df.drop(['emailtotal'], axis=1)
+        
+    value_to_index = {}
+
+    for col in df.columns.tolist():
+        if col == 'emailtotal':
+            continue
+        try:
+            unique_set = np.unique(df[col].to_numpy()).tolist()
+            unique_set = [x for x in unique_set if not math.isnan(x)]
+            unique_set.sort()
+            value_to_index[col] = dict(zip(unique_set, unique_set))
+        except:
+            #print(col, list(set(df[col].tolist())))
+            unique_set = list(set(df[col].tolist()))
+            chosens = []
+            for i in unique_set:
+                if isinstance(i, float):
+                    if math.isnan(i):
+                        continue
+                #print(type(i), i)
+                chosens.append(i)
+            unique_set = chosens
+            unique_set.sort()
+
+            if col == "advfront":
+                value_to_index[col] = {'Strongly disagree': 1, 'Disagree': 2, 'Dont know': 3, 'Agree': 4, 'Strongly agree': 5}
+            elif col == "polviews":
+                value_to_index[col] = {'Extrmly conservative': 1, 'Conservative': 2, 'Slghtly conservative': 3, 'Moderate': 4, 'Slightly liberal': 5, 'Liberal': 6, 'Extremely liberal': 7}
+            elif col == "educ" or col == "wrkstat":
+                value_to_index[col] = dict(zip(unique_set, range(len(unique_set))))
+            else:
+                value_to_index[col] = dict(zip(unique_set, range(1, len(unique_set) + 1)))
+                
+    if not use_text_categorical:
+        for col in df.columns.tolist():
+            if col == 'emailtotal' or col == "wrkstat":
+                continue
+            df[col] = df[col].map(value_to_index[col])
+            
+    categorical_columns = ['harass5', 'polviews', 'advfront', 'snapchat', 'instagrm', 'wrkstat']
+    
+    if not use_text_categorical:
+        df[categorical_columns] = df[categorical_columns].fillna(0)
+
+        df['educ'] = df['educ'].fillna(21)
+    else:
+        df[categorical_columns] = df[categorical_columns].fillna("Unknown")
+
+        df['educ'] = df['educ'].fillna("Unknown")
+    
+    if not use_text_categorical:
+        if remove_email_null:
+            casted_columns = ['harass5', 'educ', 'polviews', 'advfront', 'snapchat', 'instagrm', 'emailtotal']
+        else:
+            casted_columns = ['harass5', 'educ', 'polviews', 'advfront', 'snapchat', 'instagrm']
+
+        for col in casted_columns:
+            df[col] = df[col].astype(np.int16)
+    else:
+        if remove_email_null:
+            df['emailtotal'] = df['emailtotal'].astype(np.int16)
+            
+            
+    if one_hot_encode:
+        categorical_columns = ['harass5', 'snapchat', 'instagrm', 'polviews', 'advfront', 'educ']
+        df_encoded = pd.get_dummies(df, columns=categorical_columns, drop_first=False)
+    else:
+        df_encoded = df.copy()
+    
+    if remove_email_null:
+        numerical_columns = ['emailtotal']
+        scaler = StandardScaler()
+        df_encoded[numerical_columns] = scaler.fit_transform(df_encoded[numerical_columns])
+    
+    y = df_encoded["wrkstat"]
+    y = y.to_numpy()
+
+    x = df_encoded.drop("wrkstat", axis=1).to_numpy()
+    
+    return x, y
+
+
+# path = "./gss_16.rda"
+
+# import pyreadr
+
+# data = pyreadr.read_r(path)
+# df = data["gss16"]
+
+# x, y = preprocess_data(df=df, remove_email_null=False, use_text_categorical=False)
+
+# U, reduced_x = pca(x, alpha=0.45)
+
+# print(reduced_x, reduced_x.shape)
